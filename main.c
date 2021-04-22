7a7e564747af2d6bde74199543c914e0127af396
Mihail Iliev <mihaililiev99@gmail.com>
2021-04-22 19:24:16 +0300

    pthread main.c

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include "calc_sum.h"
+#include <pthread.h>
+
+void* threadFunc(void *arguments)
+{
+int sum;
+int *array_of_the_integers;
+array_of_the_integers = arguments;
+
+sum = calc_sum(array_of_the_integers[0], array_of_the_integers[1]);
+printf("%d\n", sum);
+return NULL;
+}
 
 int main()
 {
 int a, b, sum;
-fscanf(stdin, "%d %d", &a, &b);
-sum = calc_sum(a, b);
-printf("%d\n", sum);
+int nums[2];
+pthread_t novThread;
+fscanf(stdin, "%d %d", &nums[0], &nums[1]);
+pthread_create(&novThread, NULL, threadFunc, nums);
+pthread_join(novThread, NULL);
+//printf("%d\n", sum);
 return 0;
 }
